5118a6d873e98e80912ca85c8df56974d52f61c8
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2017-01-12 12:23:10 +0600

    add company address

--- requisition/models.py
+++ requisition/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 
 class Company(models.Model):
     name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
