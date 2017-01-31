d1c2bdf833a690a397368cc155775cae860a23ee
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2017-01-31 15:57:17 +0600

    chr11 3

--- utils/mdb.php
+++ utils/mdb.php
@@ -16,7 +16,7 @@ $stmt = $conn->query($sql);
 while ($row = $stmt->fetch()) {
 //    $data[$row['id']] = array('date' => $row['C_Date'], 'time' => $row['C_Time'], 'unique' => $row['C_Unique']);
 //    $data += array('date' => $row['C_Date'], 'time' => $row['C_Time'], 'unique' => $row['C_Unique']);
-    echo chr(11);
+    echo "0".chr(11);
     echo $row['C_Date'].chr(11);
     echo $row['C_Time'].chr(11);
     echo $row['C_Unique'].":\n";
