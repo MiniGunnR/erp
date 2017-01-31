f8d9fa873cc0029fa57406ad88478885f3905c41
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2017-01-31 12:08:37 +0600

    pages linked

--- attendance/urls.py
+++ attendance/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 
 urlpatterns = [
+    url(r'^$', views.attendance, name='attendance'),
+
     url(r'^select/by/date/$', views.select_by_date, name='select-by-date'),
-    url(r'^select/by/month/$', views.select_by_month, name='select-by-date'),
+    url(r'^select/by/month/$', views.select_by_month, name='select-by-month'),
 ]
--- attendance/views.py
+++ attendance/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 
+def attendance(request):
+    return render(request, "attendance/attendance.html")
+
+
 def select_by_date(request):
     return render(request, "attendance/select_by_date.html")
 
