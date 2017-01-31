5d88e7bfaf5e8872d8df0a10ece74d88c33609c0
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2017-01-31 12:06:45 +0600

    pages

--- attendance/views.py
+++ attendance/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def select_by_date(request):
+    return render(request, "attendance/select_by_date.html")
 
-    return render(request, "attendance/select_date.html")
+
+def select_by_month(request):
+    return render(request, "attendance/select_by_month.html")
+
