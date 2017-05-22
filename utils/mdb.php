7b4cfad173be6fea98f8cf1ad04056c220ca8449
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2017-05-22 15:50:54 +0600

    new line \r\n

--- utils/mdb.php
+++ utils/mdb.php
@@ -19,7 +19,7 @@ while ($row = $stmt->fetch()) {
 
 //    echo "0".chr(11);
     echo str_pad($row['C_Unique'], 10, "0", STR_PAD_LEFT).",";
-    echo $row['C_Date']." ".$row['C_Time']."\n";
+    echo $row['C_Date']." ".$row['C_Time']."\r\n";
 
 //    echo str_pad($row['C_Unique'], 10, "0", STR_PAD_LEFT).",";
 //    echo $row['C_Date']." ".$row['C_Time'];
