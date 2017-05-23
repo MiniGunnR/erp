4d6b3f7114516a273d8df7e317be91657b65a8fd
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2017-05-23 10:58:49 +0600

    arrays 2

--- utils/mdb.php
+++ utils/mdb.php
@@ -20,7 +20,7 @@ while ($row = $stmt->fetch()) {
 //    echo str_pad($row['C_Unique'], 10, "0", STR_PAD_LEFT).",";
 //    echo $row['C_Date']." ".$row['C_Time']."\r";
 
-    $data += array(str_pad($row['C_Unique'], 10, "0", STR_PAD_LEFT).",".$row['C_Date']." ".$row['C_Time']);
+    $data = $data + array(str_pad($row['C_Unique'], 10, "0", STR_PAD_LEFT).",".$row['C_Date']." ".$row['C_Time']);
 }
 
 header('Content-Type:application/json;charset=utf-8');
