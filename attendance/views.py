8ca27e48ea4389e903b154b6e1ecd9d852dcb5d3
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2017-05-31 12:37:38 +0600

    employees 3

--- attendance/views.py
+++ attendance/views.py
@@ -22,7 +22,7 @@ WEEKLY_OFF = calendar.SATURDAY
 
 
 def employees(request):
-    emp_ids = Profile.objects.all().values('proximity_id')['proximity_id']
+    emp_ids = Profile.objects.all().values_list('proximity_id')[0]
 
     c = {
         "emp_ids": emp_ids,
