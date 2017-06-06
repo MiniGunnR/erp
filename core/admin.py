206ff42af7b32d58a2627625dc76daf832b370b0
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2017-06-06 15:32:08 +0600

    list editable on profile

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 
 from .models import Department, Profile, Company, Designation
 
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'proximity_id',)
+    list_editable = ['proximity_id']
+
+admin.site.register(Profile, ProfileAdmin)
+
 admin.site.register(Department)
-admin.site.register(Profile)
 admin.site.register(Company)
 admin.site.register(Designation)
