a08d2cbb9913f7a0caa217421875240abe50c0a7
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2017-06-08 11:32:59 +0600

    requisition admin folder

--- core/admin.py
+++ core/admin.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
 
 from .models import Department, Profile, Company, Designation
-from django.contrib.auth.models import User
 
-
-# class UserAdmin(admin.ModelAdmin):
-#     save_on_top = True
-#
-# admin.site.unregister(User)
-# admin.site.register(User, UserAdmin)
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'proximity_id',)
