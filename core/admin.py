35a5eb12cfeb4ba4249707d97b95cf13cddc5505
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2017-06-08 11:10:43 +0600

    user save on top remove

--- core/admin.py
+++ core/admin.py
@@ -4,11 +4,11 @@ from .models import Department, Profile, Company, Designation
 from django.contrib.auth.models import User
 
 
-class UserAdmin(admin.ModelAdmin):
-    save_on_top = True
-
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+# class UserAdmin(admin.ModelAdmin):
+#     save_on_top = True
+#
+# admin.site.unregister(User)
+# admin.site.register(User, UserAdmin)
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'proximity_id',)
