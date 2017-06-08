d7a91f0222ef0432b215946e638ba0fb6c75bf3e
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2017-06-08 11:09:16 +0600

    user save on top

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 
 from .models import Department, Profile, Company, Designation
+from django.contrib.auth.models import User
 
+
+class UserAdmin(admin.ModelAdmin):
+    save_on_top = True
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'proximity_id',)
     list_editable = ['proximity_id']
+    save_on_top = True
 
 admin.site.register(Profile, ProfileAdmin)
 
