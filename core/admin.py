c0bc982f701a861c5c32702e18a447a7b0b1ef01
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2017-06-08 12:15:47 +0600

    user save redirect to profile

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 from .models import Department, Profile, Company, Designation
 from django.contrib.auth.models import User
@@ -8,6 +9,9 @@ from django.contrib.auth.admin import UserAdmin
 class UzerAdmin(UserAdmin):
     model = User
     save_on_top = True
+
+    def response_change(self, request, obj):
+        return HttpResponseRedirect('/admin/core/profile/' + str(obj.id) + '/change/')
 
 admin.site.unregister(User)
 admin.site.register(User, UzerAdmin)
