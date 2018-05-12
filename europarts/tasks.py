b11d63336d5c04aca8ba38024955e1fa74e1b0ed
MiniGunnR <hasan.mohaiminul@gmail.com>
2018-05-12 23:24:29 +0600

    absolute 2

--- europarts/tasks.py
+++ europarts/tasks.py
@@ -58,7 +58,7 @@ def generate_pdf_and_send_email(template, filename, context, pk, model, subject,
     email.from_email = from_email
     email.to = to
 
-    email.attach(file_name, pdf, 'application/pdf')
+    email.attach(os.path.join(settings.MEDIA_ROOT, file_name), pdf, 'application/pdf')
 
     email.send()
 
