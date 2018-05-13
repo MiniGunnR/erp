b84fcc705da38df4b550ef5b8382034f01cf5ba8
MiniGunnR <hasan.mohaiminul@gmail.com>
2018-05-13 20:54:36 +0600

    add pagination to mailbox 2

--- core/views.py
+++ core/views.py
@@ -108,6 +108,6 @@ def db_backup(request):
 
 class Mailbox(ListView):
     model = Mail
-    paginate_by = 10
+    paginate_by = 25
     template_name = 'core/mailbox.html'
     ordering = '-created'
